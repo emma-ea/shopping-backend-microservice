177a70975e5e65b49051fdd274382fe91051398d
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-02-12 14:55:09 +0000

    pre-populate db

--- inventory-service/src/main/java/com/emma_ea/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/emma_ea/inventory_service/InventoryServiceApplication.java
@@ -1,13 +1,35 @@
 package com.emma_ea.inventory_service;
 
+import com.emma_ea.inventory_service.model.Inventory;
+import com.emma_ea.inventory_service.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class InventoryServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Autowired
+	private InventoryRepository inventoryRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Inventory inventory = new Inventory();
+		inventory.setId(1L);
+		inventory.setSkuCode("Samsung_s5");
+		inventory.setQuantity(200);
+
+		Inventory inventory1 = new Inventory();
+		inventory1.setId(2L);
+		inventory1.setSkuCode("Samsung_s7_red");
+		inventory1.setQuantity(120);
+
+		inventoryRepository.save(inventory);
+		inventoryRepository.save(inventory1);
+	}
 }
