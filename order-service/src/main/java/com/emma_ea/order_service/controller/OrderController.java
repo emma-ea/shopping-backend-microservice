a1d64c7043e60e5b2607838195077398d0eb4c33
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-02-18 20:32:14 +0000

    impl circuit breaker fallbackmethod

--- order-service/src/main/java/com/emma_ea/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/emma_ea/order_service/controller/OrderController.java
@@ -3,7 +3,9 @@ package com.emma_ea.order_service.controller;
 import com.emma_ea.order_service.dto.OrderRequest;
 import com.emma_ea.order_service.dto.OrderResponse;
 import com.emma_ea.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +15,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String createOrder(@RequestBody OrderRequest orderRequest) {
         return orderService.createOrder(orderRequest);
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.error(runtimeException.getMessage(), runtimeException);
+        return "Oops! Something went wrong placing your order.";
     }
 
     @GetMapping
